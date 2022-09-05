fbfbd2288831477b346018d5d2bd1eba18fac56d
choidh <cdgus1514@gmail.com>
2022-09-05 23:47:59 +0900

    Initial commit

---
+++ src/main/java/rentapi/jpacar/JpacarApplication.java
@@ -0,0 +1,13 @@
+package rentapi.jpacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rentapi/jpacar/JpacarApplicationTests.java
@@ -0,0 +1,13 @@
+package rentapi.jpacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
